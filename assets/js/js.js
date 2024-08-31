cb482468a38c7f340a5cd435f0cc518c1cb3a348
Wesley Soares <wesley@tecnoig.com.br>
2024-08-31 16:41:08 -0300

    feat(components) adicionando novas classes carousel

--- assets/js/js.js
+++ assets/js/js.js
@@ -11,63 +11,68 @@ $(document).ready(function () {
         { id: 9, title: 'Book Title 9', image: 'https://picsum.photos/150?9' }
     ];
 
-    const $carouselWrapper = $('.book-carousel__wrapper');
+    const $carouselWrapper = $('.carousel__wrapper');
     let itemWidth = 0;
     let currentPosition = 0;
 
     function hideLoadingShowCarousel() {
         $('#loading').hide();
-        $('.book-carousel-container').fadeIn();
+        $('.carousel-container').fadeIn();
     }
 
     function renderCarouselItems() {
         $carouselWrapper.empty();
         books.forEach(book => {
             const itemHtml = `
-                <div class="book-carousel__item" data-id="${book.id}">
-                    <img src="${book.image}" alt="${book.title}" class="book-carousel__image">
-                    <p class="book-carousel__title">${book.title}</p>
+                <div class="carousel__item" data-id="${book.id}">
+                    <img src="${book.image}" alt="${book.title}" class="carousel__image">
+                    <p class="carousel__title">${book.title}</p>
                 </div>
             `;
             $carouselWrapper.append(itemHtml);
         });
-        itemWidth = $('.book-carousel__item').outerWidth(true);
+        itemWidth = $('.carousel__item').outerWidth(true);
     }
 
     function updateCarouselPosition() {
+        const maxPosition = -($carouselWrapper.children().length * itemWidth - $('.carousel').width());
+        currentPosition = Math.max(currentPosition, maxPosition);  // Evita deslizar além do último item
         $carouselWrapper.css('transform', `translateX(${currentPosition}px)`);
     }
 
-    $('.book-carousel__control--next').click(function () {
-        const maxPosition = -($carouselWrapper.children().length - Math.floor($('.book-carousel').width() / itemWidth)) * itemWidth;
+    function adjustCarouselOnResize() {
+        itemWidth = $('.carousel__item').outerWidth(true);
+        currentPosition = Math.max(currentPosition, -($carouselWrapper.children().length * itemWidth - $('.carousel').width()));
+        updateCarouselPosition();
+    }
+
+    $('.carousel__control--next').click(function () {
+        const maxPosition = -($carouselWrapper.children().length * itemWidth - $('.carousel').width());
         if (currentPosition > maxPosition) {
             currentPosition -= itemWidth;
             updateCarouselPosition();
         }
     });
 
-    $('.book-carousel__control--prev').click(function () {
+    $('.carousel__control--prev').click(function () {
         if (currentPosition < 0) {
             currentPosition += itemWidth;
             updateCarouselPosition();
         }
     });
 
-    $(document).on('click', '.book-carousel__image', function () {
-        const bookId = $(this).closest('.book-carousel__item').data('id');
+    $(document).on('click', '.carousel__image', function () {
+        const bookId = $(this).closest('.carousel__item').data('id');
         const baseUrl = window.location.origin + window.location.pathname.replace('index.html', ''); 
-            if (bookId) {
+        if (bookId) {
             window.location.href = `${baseUrl}book-details.html?id=${encodeURIComponent(bookId)}`;
         } else {
             console.error('Book ID not found');
         }
     });
     
-    
     $(window).resize(function () {
-        itemWidth = $('.book-carousel__item').outerWidth(true);
-        currentPosition = Math.max(currentPosition, -($carouselWrapper.children().length - Math.floor($('.book-carousel').width() / itemWidth)) * itemWidth);
-        updateCarouselPosition();
+        adjustCarouselOnResize();
     });
 
     renderCarouselItems();
